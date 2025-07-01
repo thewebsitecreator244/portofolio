896373fc66ca9372925e3b0e0c5438ee26b6416f
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-07-01 07:04:17 +0200

    feat:adaptted thhe avatar fully

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,11 @@
 const BURGER = document.querySelector("#burger");
 const MENU = document.querySelector("#menu");
-
+const ANCHOR = document.querySelector("#link-anchor")
 BURGER.addEventListener("click",()=>{
     BURGER.classList.toggle("open");
-    MENU.classList.toggle("open")
+    MENU.classList.toggle("open");
+})
+ANCHOR.addEventListener("click",()=>{
+    BURGER.classList.toggle("open");
+    MENU.classList.toggle("open");
 })
