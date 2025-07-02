30d299db4a9db3977b6ebca2d39a327504966716
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-07-02 07:41:34 +0200

    refactor:changed burger code using data and event attributes

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,20 @@
-const BURGER = document.querySelector("#burger");
-const MENU = document.querySelector("#menu");
-const ANCHOR = document.querySelector("#link-anchor")
-BURGER.addEventListener("click",()=>{
-    BURGER.classList.toggle("open");
-    MENU.classList.toggle("open");
-})
-// added menu closing on link press
-ANCHOR.addEventListener("click",()=>{
-    BURGER.classList.toggle("open");
-    MENU.classList.toggle("open");
-})
+const HEADER = document.querySelector("#main-header");
+const MENU = HEADER.querySelector("#menu");
+
+function toggle(event){
+    let isBurger = event.target.closest("#burger");
+    let isAnchor = event.target.closest("[data-anchor]");
+
+    if (isBurger){
+        MENU.classList.toggle("open");
+        isBurger.classList.toggle("open");
+    }
+    
+    if (isAnchor){
+        MENU.classList.remove("open");
+        isBurger = HEADER.querySelector("#burger")
+        isBurger.classList.remove("open");
+    }
+}
+
+HEADER.addEventListener("click",(event) => toggle(event));
