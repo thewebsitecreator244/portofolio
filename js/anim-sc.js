486fd73a1abd34b93d0bb7c18b74d489b7ae6ae8
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-08-13 07:52:17 +0200

    feat:finished card scripting

--- js/anim-sc.js
+++ js/anim-sc.js
@@ -1,13 +1,20 @@
 const CARDLIST = document.querySelector("#skill-list")
+const FADE = CARDLIST.closest("#skills").querySelector("#fadeblock")
 
 function cardAnim(event){
     const isCard = event.target.closest("[data-skill-card]")
+    const isClose = event.target.closest("[data-skill-card-close]")
     if (isCard) {
         isCard.classList.add("active")
+        FADE.classList.add("active")
     }
-    
-    
-
+    if (isClose) {
+        const parent = isClose.closest("[data-skill-card]")
+        if (parent){
+            parent.classList.remove("active")
+            FADE.classList.remove("active")
+        }
+    }
 }
 
 CARDLIST.addEventListener("click",(event) => cardAnim(event))
