eca5c55d3cbcd1bf28417ccb582da97e189c2884
thewebsitecreator244 <alexkudinov1300@gmail.com>
2025-10-02 14:48:02 +0200

    hn

--- js/gsapanim.js
+++ js/gsapanim.js
@@ -9,15 +9,39 @@ gsap.from("#heroTitle",
     }
 );
 
-const tl = gsap.timeline({
+let tl = gsap.timeline({
     scrollTrigger: {
-        trigger: "#aboutMe",
+        trigger: "#about",
         start: "27% 80%",
         end: "32% 80%",
         scrub: 1,
         markers:true
     }
 });
+
+let mobileWidthMediaQuery = window.matchMedia('(max-width: 768px)')
+console.log(mobileWidthMediaQuery);
+
+
+function changeMarkers(isMobileSize) {
+    if (isMobileSize){
+        tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "#about",
+                start: "45% 80%",
+                end: "32% 80%",
+                scrub: 1,
+                markers:true
+            }
+        });
+    }
+}
+
+changeMarkers(mobileWidthMediaQuery.matches)
+
+mobileWidthMediaQuery.addEventListener('resize', function (event) {
+    changeMarkers(event.matches)
+})
 
 const split = new SplitText("#aboutText", { type: "chars", mask: "chars" });
 
